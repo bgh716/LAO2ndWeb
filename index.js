b930412564206bd5f6e2a3f73e621a9458479e04
bgh716 <kbyun@sfu.ca>
2022-08-15 00:22:20 -0700

    dockerfile for nginx

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 80
 
 var app = express()
 
-app.get('/hello', (req, res) => {
-  res.end('Hello World!')
+app.get('/'', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
