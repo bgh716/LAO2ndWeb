1d304f24b8556a5b335ce04fb7c8d0bdccda5c3b
bgh716 <kbyun@sfu.ca>
2022-08-14 22:01:00 -0700

    hello world!

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3000
 
 var app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(PORT, () => {
   console.log('서버가 http://34.125.169.162:' + PORT + '에서 작동하고 있습니다.');
 })
