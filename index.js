90b65a663176ae964c57e5ddd55c6092da3bb6c4
bgh716 <kbyun@sfu.ca>
2022-08-18 00:52:14 -0700

    react code built for front end

--- index.js
+++ index.js
@@ -1,11 +1,162 @@
 var express = require('express')
+const fs = require('fs');
+const path = require('path')
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000
+const DB_URI = process.env.DB_URI
 
 var app = express()
 
+//MONGOOSE CONNECTION
+const mongoose = require('mongoose')
+mongoose.connect(
+  DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "LAO",
+  }
+);
+const db = mongoose.connection;
+const handleOpen = () => {
+    console.log(`✅ Connected to DB`);
+};
+const handleError = (error) => {
+    console.log(`❌ Error on DB connection: ${error}`);
+};
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+var maris = mongoose.Schema({
+    "id" : String,
+    "crystal" : Number,
+    "i1" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i2" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i3" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i4" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i5" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i6" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i7" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i8" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i9" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i10" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i11" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i12" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i13" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i14" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i15" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i16" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i17" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i18" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i19" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i20" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i21" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i22" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i23" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i24" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i25" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i26" : {
+        "name" : String,
+        "price" : Number
+    },
+    "i27" : {
+        "name" : String,
+        "price" : Number
+    }
+});
+const Maris = mongoose.model('Maris', maris);
+
+const infoFile = fs.readFileSync('temp/iteminfo.json', 'utf8');
+const infoObj = JSON.parse(infoFile);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/shop', (req, res) => {
+  Maris.find({id:"TEMPO"}, function (err, docs) {
+    if (err){
+      console.log(err);
+    }
+    //const tmpObj = docs[0];
+    res.json({data: docs[0], info: infoObj});
+  });
 })
 
 app.listen(PORT, () => {
--- lao-react/src/App.js
+++ lao-react/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const columns = ["item", "gold price", "crystal price", "rate(shows how much crystal is more expensive)"];
+  //axios callback function
+  const [res, setRes] = useState(null);
+  function getData(){
+    axios.get("http://localhost:80/shop")
+      .then(function (response) {
+           setRes(response.data)
+      }).catch(function (error) {
+          console.log(error)
+      });
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(res)
+
+  return "Hello World!";
 }
 
 export default App;
